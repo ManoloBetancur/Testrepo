2572f5de7dab84888cce262bc6a2f39c9ada7194
Manolo Betancur <olonammanolo@gmail.com>
2021-01-31 21:05:54 -0500

    Second Commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello world!")
+    print("Change made")
+
+if __name__ == '__main__':
+    main()
---
+++ more.py
@@ -0,0 +1,6 @@
+def main():
+    for e in range(10):
+        print(e)
+
+if __name__ == '__main__':
+    main()
